4ea6e57f4760dfcb9edbc2b984949e9f1acf1b03
chenxy <1063975796@qq.com>
2017-02-16 15:06:47 +0800

    develop1

--- firstPro/src/com/cmsz/Hi.java
+++ firstPro/src/com/cmsz/Hi.java
@@ -4,7 +4,7 @@ public class Hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi, girlsss");
+		System.out.println("Hi, develop1");
 	}
 
 }
