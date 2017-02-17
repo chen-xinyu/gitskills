1983c4507363715ad79604932e198fcf807c90ee
chenxy <1063975796@qq.com>
2017-02-17 10:31:59 +0800

    develop1.3

--- firstPro/src/com/cmsz/Hi.java
+++ firstPro/src/com/cmsz/Hi.java
@@ -4,7 +4,7 @@ public class Hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi, develop1.2");
+		System.out.println("Hi, develop1.3");
 	}
 
 }
